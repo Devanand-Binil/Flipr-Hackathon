4bc5101006d23b211ddae47f10f0a422c217bb2e
TheBatOfGotham <joshuajk5328@gmail.com>
2025-05-24 12:52:36 +0530

    auth almost

---
+++ Flipr-Hackathon-FE/src/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import * as process from "process";
+
+// Polyfills for some Node globals in the browser
+window.global = window;
+window.process = process;
+window.Buffer = [];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
